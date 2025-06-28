a4f136131a6a9ca1dd11335b4a3465231776bef6
Saurav <sauravganc@gmail.com>
2025-06-29 01:00:24 +0530

    Initial commit - churn predictor app

---
+++ main.py
@@ -0,0 +1,61 @@
+# loading necessary libraries
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+import joblib, json, pandas as pd
+from pathlib import Path
+
+# loading artifacts, meaning at start-up we read the model, scaler, feature list, and threshold from disk once, so predictions are fast.
+BASE_DIR = Path(__file__).resolve().parent
+PIPELINE_DIR = BASE_DIR / "saved_pipeline"
+
+model = joblib.load(PIPELINE_DIR / "rf_model.pkl")
+scaler = joblib.load(PIPELINE_DIR / "rf_scaler.pkl")
+
+with open(PIPELINE_DIR / "rf_features.json") as f:
+    Features = json.load(f)
+    
+with open(PIPELINE_DIR / "rf_threshold.json") as f:
+    Threshold = json.load(f)
+
+# defining the request and response scripts/shapes
+class churnRequestBody(BaseModel):
+    credit_score: int = Field(..., alias = "CreditScore", description="Credit score of the customer")
+    geography: str
+    gender: str
+    age: int = Field(..., ge=18, le=80, alias = "Age", description="Age of the customer")
+    tenure: int = Field(ge=0, le=10)
+    balance: float = Field(ge = 0)
+    num_of_products: int = Field(ge=1, le=4, alias = "NumOfProducts", description="Number of products the customer has")
+    has_credit_card: bool = Field(alias = "HasCrCard", description="Does the customer have a credit card?")
+    is_active_member: bool = Field(alias = "IsActiveMember", description="Is the customer an active member?")
+    estimated_salary: float = Field(ge=0, alias = "EstimatedSalary", description="Estimated salary of the customer")
+    
+class churnResponseBody(BaseModel):
+    churn_probability: float
+    will_churn: bool
+    
+# creating fastapi app
+app = FastAPI(title="Churn Prediction API")
+
+@app.get("/")
+def root_route():
+    return {"msg":"Churn Prediction API is live 🎊"}
+
+@app.post("/predict", response_model = churnResponseBody)
+def predict(body: churnRequestBody):
+    print(body.dict(by_alias=True))
+    try:
+        #1. convert the input data into dataframe and turn the json into Python dictionary with exact keys like hasCreditCard.
+        df = pd.DataFrame([body.dict(by_alias=True)]).reindex(columns=Features)
+        
+        #2. scaling it like we did while training
+        x_scaled = scaler.transform(df)
+        
+        #3.predict churn probability
+        prob = float(model.predict_proba(x_scaled)[0][1])
+        will_churn = prob > Threshold
+        
+        return churnResponseBody(churn_probability=round(prob, 3), will_churn=will_churn)              
+                      
+    except Exception as e:
+        raise HTTPException(status_code = 500, detail=f'Inference error: {e}')
---
+++ streamlit_app.py
@@ -0,0 +1,50 @@
+import streamlit as st # lets you build the web UI (form, buttons, sliders).
+import requests
+
+# Adds a page title at the top of the Streamlit app.
+st.title("Churn Prediction API")
+st.set_page_config(layout="wide") # sets the layout of the page to wide, so that it takes the full width of the browser window.
+# FastAPI endpoint URL
+API_URL = "http://127.0.0.1:8000/predict" # This is where Streamlit will send the input data to get a result.
+
+# Building the input form
+with st.form("churn_form"):
+    credit_score = st.number_input("Credit Score", min_value=300, max_value=850, value=650) # value 650 is default value
+    geography = st.selectbox("Geography", ["France", "Spain", "Germany"]) # dropdown
+    gender = st.selectbox('Gender', ["Male", "Female"]) # dropdown
+    age = st.slider("Age", min_value=18, max_value=80, value=30) # slider. default value is 30
+    tenure = st.slider("Tenure (Years)", min_value=0, max_value=10, value=5) # slider. default value is 5
+    balance = st.number_input("Balance", min_value=0.0, max_value=1000000.0, value=125000.0) # default value is 10000
+    num_of_products = st.slider("Number of Products", 1, 4, 2)  # slider. default value is 2. automatically takes the min, max and value
+    has_credit_card = st.checkbox("Has Credit Card") # checkbox
+    is_active_member = st.checkbox("Is Active Member") # checkbox
+    estimated_salary = st.number_input("Estimated Salary", min_value=0.0, value=50000.0) # default value is 50000
+    
+    submitted = st.form_submit_button("Predict Churn")
+    
+# After clicking the Predict button:
+if submitted:
+    # Prepare the input data as a dictionary
+    input_data = {
+        "CreditScore": credit_score,
+        "geography": geography,
+        "gender": gender,
+        "Age": age,
+        "tenure": tenure,
+        "balance": balance,
+        "NumOfProducts": num_of_products,
+        "HasCrCard": has_credit_card,
+        "IsActiveMember": is_active_member,
+        "EstimatedSalary": estimated_salary
+    }
+    
+    try:
+        response = requests.post(API_URL, json = input_data, timeout = 5)
+        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+        result = response.json()  # Parse the JSON response
+        
+        st.success(f'Churn Probability: {result["churn_probability"]}, Will Churn: {result["will_churn"]}')
+    
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error: {e}")   
+        
